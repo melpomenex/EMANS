0b2051aacb0cc27d9d0a3f0c811957c6d5e57464
Ben <gnomer@gmail.com>
2018-07-13 06:12:48 -0600

    add rudimentary login functionality

--- server/main.go
+++ server/main.go
@@ -1,40 +1,112 @@
 package main
 
 import (
-	"html/template"
-	"io"
-	"log"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
 )
 
-var tpl *template.Template
+// cookie handling
 
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func getUserName(request *http.Request) (userName string) {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["name"]
+		}
+	}
+	return userName
 }
 
-type loginDetails struct {
-	Username string
-	Password string
-}
-
-func login(w http.ResponseWriter, req *http.Request) {
-	u := req.FormValue("username")
-	p := req.FormValue("password")
-
-	err := tpl.ExecuteTemplate(w, "login.gohtml", loginDetails{u, p})
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		log.Fatalln(err)
+func setSession(userName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"name": userName,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(response, cookie)
 	}
 }
 
-func index(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "cat cat cat")
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
 }
 
+// login handler
+
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("name")
+	pass := request.FormValue("password")
+	redirectTarget := "/"
+	if name != "" && pass != "" {
+		// .. check credentials ..
+		setSession(name, response)
+		redirectTarget = "/internal"
+	}
+	http.Redirect(response, request, redirectTarget, 302)
+}
+
+// logout handler
+
+func logoutHandler(response http.ResponseWriter, request *http.Request) {
+	clearSession(response)
+	http.Redirect(response, request, "/", 302)
+}
+
+// index page
+
+func indexPageHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, LoginHTML)
+}
+
+// internal page
+
+const internalPage = `
+<h1>Internal</h1>
+<hr>
+<small>User: %s</small>
+<form method="post" action="/logout">
+    <button type="submit">Logout</button>
+</form>
+`
+
+func internalPageHandler(response http.ResponseWriter, request *http.Request) {
+	userName := getUserName(request)
+	if userName != "" {
+		fmt.Fprintf(response, internalPage, userName)
+	} else {
+		http.Redirect(response, request, "/", 302)
+	}
+}
+
+// server main method
+
+var router = mux.NewRouter()
+
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/login", login)
-	http.ListenAndServe(":8080", nil)
+
+	router.HandleFunc("/", indexPageHandler)
+	router.HandleFunc("/internal", internalPageHandler)
+
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/logout", logoutHandler).Methods("POST")
+
+	http.Handle("/", router)
+	http.ListenAndServe(":8000", nil)
 }
